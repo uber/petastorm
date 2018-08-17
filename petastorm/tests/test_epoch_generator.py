f9d76685124603cc1e5a27dc77aec80b207bb769
Yevgeni Litvin <yevgeni@uber.com>
2018-08-17 15:22:20 -0700

    [Part 1] New reader flow: Epoch generator
    Epoch generator is factored out of ventilator.py and will be used in the reader_v2

---
+++ petastorm/tests/test_epoch_generator.py
@@ -0,0 +1,44 @@
+from petastorm.reader_impl.epochs import epoch_generator
+
+
+def test_one_epoch():
+    assert list(epoch_generator([1, 2, 3], 1, False)) == [1, 2, 3]
+
+
+def test_three_epochs():
+    assert list(epoch_generator([1, 2, 3], 3, False)) == [1, 2, 3, 1, 2, 3, 1, 2, 3]
+
+
+def test_shuffle():
+    items_in_epoch = 50
+    shuffled_epoch = list(epoch_generator(list(range(items_in_epoch)), 1, True))
+    assert len(shuffled_epoch) == items_in_epoch
+    assert shuffled_epoch != range(items_in_epoch)
+
+
+def test_shuffle_per_epoch():
+    items_in_epoch = 50
+    two_epochs = list(epoch_generator(list(range(items_in_epoch)), 2, True))
+    assert len(two_epochs) == 100
+    assert two_epochs[:50] != two_epochs[50:]
+
+
+def test_empty_items_list():
+    assert not list(epoch_generator([], 2, True))
+    assert not list(epoch_generator([], None, True))
+
+
+def test_item_type_can_be_anything():
+    assert list(epoch_generator([None, 'a string', {}], 1, False)) == [None, 'a string', {}]
+
+
+def test_unlimited_epochs():
+    items = [1, 2, 3]
+    inf_epochs = epoch_generator(items, None, True)
+    for _ in range(1000):
+        actual_item = next(inf_epochs)
+        assert actual_item in items
+
+
+def test_one_item_epoch():
+    assert list(epoch_generator([1, ], 3, False)) == [1, 1, 1]
