aca98fb88ad61052b482e2019c2f64842beda95a
Owen Cheng <ocheng@uberatc.com>
2018-08-21 16:16:28 -0400

    New pytorch tests and addressing review comments:
    * pass in Reader instance to DataLoader adapter
    * pin pip version
    * fix class reference in comment
    * early import of torch in conftest to prevent dlopen error for Python < 3.0
    * skip of test_read_mnist_dataset and test_full_pytorch_example when Python < 3.0
    * separate out test_generate_mnist_dataset, which can run in both Python versions

---
+++ petastorm/tests/test_pytorch_utils.py
@@ -0,0 +1,47 @@
+#  Copyright (c) 2017-2018 Uber Technologies, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import division
+
+from petastorm.pytorch import DataLoader
+from petastorm.reader import Reader
+from petastorm.tests.conftest import ROWS_COUNT
+from petastorm.workers_pool.dummy_pool import DummyPool
+
+
+def _noop_collate(alist):
+    return alist
+
+
+def test_basic_pytorch_dataloader(synthetic_dataset):
+    loader = DataLoader(Reader(synthetic_dataset.url, reader_pool=DummyPool()), collate_fn=_noop_collate)
+    assert len(loader) == ROWS_COUNT
+    for item in loader:
+        assert len(item) == 1
+
+
+def test_pytorch_dataloader_batched(synthetic_dataset):
+    batch_size = 10
+    loader = DataLoader(Reader(synthetic_dataset.url, reader_pool=DummyPool()),
+                        batch_size=batch_size, collate_fn=_noop_collate)
+    assert len(loader) == ROWS_COUNT / batch_size
+    for item in loader:
+        assert len(item) == batch_size
+
+
+def test_pytorch_dataloader_context(synthetic_dataset):
+    with DataLoader(Reader(synthetic_dataset.url, reader_pool=DummyPool()),
+                    collate_fn=_noop_collate) as loader:
+        assert len(loader) == ROWS_COUNT
+        for item in loader:
+            assert len(item) == 1
