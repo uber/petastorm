d164d0e83ea8b71c1e4cd209f5a48ad62bdf3120
Yevgeni Litvin <yevgeni@uber.com>
2018-11-20 17:07:50 -0800

    Port test_disk_cache to use pytest

--- petastorm/tests/test_disk_cache.py
+++ petastorm/tests/test_disk_cache.py
@@ -13,12 +13,10 @@
 # limitations under the License.
 
 import os
-import unittest
 
 import numpy as np
 
 from petastorm.local_disk_cache import LocalDiskCache
-from petastorm.tests.tempdir import temporary_directory
 
 MB = 2 ** 20
 KB = 2 ** 10
@@ -33,32 +31,24 @@ def _recursive_folder_size(folder):
     return folder_size
 
 
-class TestDiskCache(unittest.TestCase):
-
-    def test_simple_scalar_cache(self):
-        """Testing trivial NullCache: should trigger value generating function on each run"""
-        # with temporary_directory() as cache_dir:
-        #     cache = LocalDiskCache(cache_dir, 1 * MB, 4)
-        #     self.assertEqual(42, cache.get('some_key', lambda: 42))
-        #     self.assertEqual(42, cache.get('some_key', lambda: 43))
-
-    def test_size_limit_constraint(self):
-        """Testing trivial NullCache: should trigger value generating function on each run"""
-        with temporary_directory() as cache_dir:
-            # We will write total of 5MB to the cache (50KB items x 100)
-            RECORD_SIZE_BYTES = 50 * KB
-            RECORDS_COUNT = 100
-
-            a_record = np.random.randint(0, 255, (RECORD_SIZE_BYTES,), np.uint8)
-            cache = LocalDiskCache(cache_dir, 1 * MB, RECORD_SIZE_BYTES, shards=1)
-
-            for i in range(RECORDS_COUNT):
-                cache.get('some_key_{}'.format(i), lambda: a_record)
-
-            # Check that we are more or less within the size limit
-            self.assertLess(_recursive_folder_size(cache_dir), 2 * MB)
+def test_simple_scalar_cache(tmpdir):
+    """Testing trivial NullCache: should trigger value generating function on each run"""
+    cache = LocalDiskCache(tmpdir.strpath, 1 * MB, 4)
+    assert 42 == cache.get('some_key', lambda: 42)
+    assert 42 == cache.get('some_key', lambda: 43)
 
 
-if __name__ == '__main__':
-    # Delegate to the test framework.
-    unittest.main()
+def test_size_limit_constraint(tmpdir):
+    """Testing trivial NullCache: should trigger value generating function on each run"""
+    # We will write total of 5MB to the cache (50KB items x 100)
+    RECORD_SIZE_BYTES = 50 * KB
+    RECORDS_COUNT = 100
+
+    a_record = np.random.randint(0, 255, (RECORD_SIZE_BYTES,), np.uint8)
+    cache = LocalDiskCache(tmpdir.strpath, 1 * MB, RECORD_SIZE_BYTES, shards=1)
+
+    for i in range(RECORDS_COUNT):
+        cache.get('some_key_{}'.format(i), lambda: a_record)
+
+    # Check that we are more or less within the size limit
+    assert _recursive_folder_size(tmpdir.strpath) < 2 * MB
