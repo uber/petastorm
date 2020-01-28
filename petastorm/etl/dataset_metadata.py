d22de8bb4d80e88766a06b4f07d3b71ef9025e63
Yevgeni Litvin <yevgeni@uber.com>
2020-01-28 01:00:12 -0800

    Fix incorrect counting of number of row-groups per piece.
    This bug manifests in either:
    - `pyarrow.lib.ArrowIOError: The file only has <X> row groups, requested metadata for row group: <Y>` (where <X> and <Y> are integers) exception, or:
    - silently - some rows in a dataset might have been silently ignored.
    
    This issue occured when:
    - using `make_batch_reader`
    - number of row-groups in different parquet files (pieces) is different.
    
    The issue was in introduced in petastorm 0.7.7

--- petastorm/etl/dataset_metadata.py
+++ petastorm/etl/dataset_metadata.py
@@ -312,6 +312,14 @@ def _split_row_groups(dataset):
     return split_pieces
 
 
+def _split_piece(piece, fs_open):
+    metadata = compat_get_metadata(piece, fs_open)
+    return [compat_make_parquet_piece(piece.path, fs_open,
+                                      row_group=row_group,
+                                      partition_keys=piece.partition_keys)
+            for row_group in range(metadata.num_row_groups)]
+
+
 def _split_row_groups_from_footers(dataset):
     """Split the row groups by reading the footers of the parquet pieces"""
 
@@ -322,14 +330,7 @@ def _split_row_groups_from_footers(dataset):
 
     thread_pool = futures.ThreadPoolExecutor()
 
-    def split_piece(piece):
-        metadata = compat_get_metadata(dataset.pieces[0], dataset.fs.open)
-        return [compat_make_parquet_piece(piece.path, dataset.fs.open,
-                                          row_group=row_group,
-                                          partition_keys=piece.partition_keys)
-                for row_group in range(metadata.num_row_groups)]
-
-    futures_list = [thread_pool.submit(split_piece, piece) for piece in dataset.pieces]
+    futures_list = [thread_pool.submit(_split_piece, piece, dataset.fs.open) for piece in dataset.pieces]
     result = [item for f in futures_list for item in f.result()]
     thread_pool.shutdown()
     return result
