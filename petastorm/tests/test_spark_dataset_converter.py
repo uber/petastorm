e1fac8f5394da6c30d86e41de775ba0fb5a1fb35
WeichenXu <weichen.xu@databricks.com>
2020-03-16 17:30:49 +0800

    Revert "fix"
    This reverts commit 5de4b8b552058a7a44a7727db73469d75569115d.

--- petastorm/tests/test_spark_dataset_converter.py
+++ petastorm/tests/test_spark_dataset_converter.py
@@ -27,7 +27,7 @@ from pyspark.sql.types import (BinaryType, BooleanType, ByteType, DoubleType,
                                StringType, StructField, StructType)
 from six.moves.urllib.parse import urlparse
 
-import petastorm
+from petastorm import make_batch_reader
 from petastorm.fs_utils import FilesystemResolver
 from petastorm.spark import make_spark_converter
 from petastorm.spark import spark_dataset_converter
@@ -258,19 +258,19 @@ def test_tf_dataset_batch_size(test_ctx):
 @contextmanager
 def mock_make_batch_reader():
     captured_args = []
-    original_make_batch_reader = petastorm.make_batch_reader
+    import petastorm.spark
 
     def mock_fn(dataset_url, **kwargs):
         reader_args = {'dataset_url': dataset_url}
         reader_args.update(kwargs)
         captured_args.append(reader_args)
-        return original_make_batch_reader(dataset_url, **kwargs)
+        return make_batch_reader(dataset_url, **kwargs)
 
-    petastorm.make_batch_reader = mock_fn
+    petastorm.spark.spark_dataset_converter.make_batch_reader = mock_fn
     try:
         yield captured_args
     finally:
-        petastorm.make_batch_reader = original_make_batch_reader
+        petastorm.spark.spark_dataset_converter.make_batch_reader = make_batch_reader
 
 
 def test_tf_dataset_petastorm_args(test_ctx):
