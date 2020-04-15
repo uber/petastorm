83a02dfdded932adbc1fc7d6faf2df1f3a4a6cac
WeichenXu <weichen.xu@databricks.com>
2020-04-15 14:18:04 +0800

    CI with Tensorflow 2.1 (#536)
    Now CI run on following env:
      - PYARROW_VERSION=0.12.1 TF_VERSION=1.15.0 DEPLOYABLE=0 PY=2.7 PYSPARK_VERSION=2.4.4
      - PYARROW_VERSION=0.15.1 TF_VERSION=1.15.0 DEPLOYABLE=0 PY=2.7 PYSPARK_VERSION=2.4.4 ARROW_PRE_0_15_IPC_FORMAT=1
      - PYARROW_VERSION=0.12.1 TF_VERSION=1.15.0 DEPLOYABLE=0 PY=3.7 PYSPARK_VERSION=2.4.4
      - PYARROW_VERSION=0.15.1 TF_VERSION=1.15.0 DEPLOYABLE=1 PY=3.7 PYSPARK_VERSION=2.4.4 ARROW_PRE_0_15_IPC_FORMAT=1
      - PYARROW_VERSION=0.15.1 TF_VERSION=1.15.0 DEPLOYABLE=0 PY=3.7 PYSPARK_VERSION=3.0.0 ARROW_PRE_0_15_IPC_FORMAT=0
      - PYARROW_VERSION=0.15.1 TF_VERSION=2.1.0 DEPLOYABLE=0 PY=3.7 PYSPARK_VERSION=3.0.0 ARROW_PRE_0_15_IPC_FORMAT=0
    
    I replace all `import tensorflow as tf` with `import tensorflow.compat.v1 as tf`, so that all TF v1 API can be run on tensorflow 2.
    
    Some changes on test:
    * I wrap all TF test with `@create_tf_graph_if_tf2 ` (Creating default tf.graph() explicitly is required by TF2 with v1 compatible API)
    
    Some change on source code:
    * I update `petastorm.tf_utils._set_shape`,  add a check only when `fields_as_dict[k].get_shape().dims is None` then set shape for tensor. Because TF2 (default eager mode) sometimes will provide accurate runtime shape so we do not need to set it.

--- petastorm/spark/spark_dataset_converter.py
+++ petastorm/spark/spark_dataset_converter.py
@@ -311,7 +311,7 @@ class TFDatasetContextManager(object):
     def __enter__(self):
         # import locally to avoid importing tensorflow globally.
         from petastorm.tf_utils import make_petastorm_dataset
-        import tensorflow as tf
+        import tensorflow.compat.v1 as tf  # pylint: disable=import-error
 
         _wait_file_available(self.parquet_file_url_list)
         self.reader = make_batch_reader(self.parquet_file_url_list, **self.petastorm_reader_kwargs)
