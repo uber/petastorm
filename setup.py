22d9f1a67e5352587fcf13e6926e51f4396e5a94
Dave McGuire <dmcguire@blackberry.com>
2020-09-28 22:42:07 -0700

    Enforce the minimal s3fs version at which S3FSWrapper is not needed

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ EXTRA_REQUIRE = {
         'alabaster>=0.7.11'
     ],
     'opencv': ['opencv-python>=3.2.0.6'],
+    's3fs': ['s3fs>=0.5.0'],
     'tf': ['tensorflow>=1.14.0'],
     'tf_gpu': ['tensorflow-gpu>=1.14.0'],
     'test': [
