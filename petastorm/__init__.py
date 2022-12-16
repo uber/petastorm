d2669ecc6a4b08403d7b8527258f7dd81f8da03e
Yevgeni Litvin <yevgeni@aurora.tech>
2022-12-15 21:54:05 -0800

    0.12.1rc1

--- petastorm/__init__.py
+++ petastorm/__init__.py
@@ -16,4 +16,4 @@ from petastorm.errors import NoDataAvailableError  # noqa: F401
 from petastorm.reader import make_reader, make_batch_reader  # noqa: F401
 from petastorm.transform import TransformSpec  # noqa: F401
 
-__version__ = '0.12.0'
+__version__ = '0.12.1rc1'
